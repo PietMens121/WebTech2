9b7ee21da0c9bbb7e2584f3ee4402edf568df449
Erwin-Veenhoven <erwinveenhoven2907@gmail.com>
2023-04-03 15:48:06 +0200

    changed root file to \public

--- public/index.php
+++ public/index.php
@@ -4,20 +4,20 @@
 define('BASE_PATH', realpath(dirname(__FILE__)));
 
 spl_autoload_register(function ($class) {
-    $filename = BASE_PATH .'/'. str_replace('\\', '/', $class) . '.php';
+    $filename = '../'. str_replace('\\', '/', $class) . '.php';
     include($filename);
 });
 
 //Autoload the psr classes
-require BASE_PATH."/vendor/autoload.php";
+require "../vendor/autoload.php";
 
 session_start();
 
 use App\Routing\Router;
 use App\Service\DotEnv;
 
-(new DotEnv(__DIR__ . '/.env'))->load();
+(new DotEnv('../.env'))->load();
 
 //Router
-require_once BASE_PATH . '/routes/web.php';
+require_once '../routes/web.php';
 Router::getInstance()->handleRequest();
